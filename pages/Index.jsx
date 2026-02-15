88052cc8eecb6e36e87ee700113c465e0345093e
Preet Singh <preet10syd@gmail.com>
2026-02-15 13:59:34 +1100

    fix: the cart items are properly counted and displayed

--- pages/Index.jsx
+++ pages/Index.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 import NavBar from "../src/components/Navbar";
 import { Outlet } from "react-router-dom";
 
-function Index(){
-    
-const [useCart, setCart] = useState([])
+function Index() {
+
+    const [useCart, setCart] = useState([])
+    const cartItems = useCart.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue.times
+    }, 0
+    )
     return (
         <>
-        <NavBar cartLength ={useCart.length}/>
-       <Outlet context={[useCart, setCart]}/> 
+            <NavBar cartLength={cartItems} />
+            <Outlet context={[useCart, setCart]} />
         </>
     )
 }
